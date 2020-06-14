903d5fafc95431d6ffd1b90ddb2f19d1a5ac2916
sweetcoral <g2855322030@163.com>
2020-06-14 22:49:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+import TIM from 'tim-wx-sdk';
+import COS from "cos-wx-sdk-v5";
+
+let options = {
+  SDKAppID: 1400380651 // 接入时需要将0替换为您的即时通信 IM 应用的 SDKAppID
+};
+// 创建 SDK 实例，`TIM.create()`方法对于同一个 `SDKAppID` 只会返回同一份实例
+let tim = TIM.create(options); // SDK 实例通常用 tim 表示
+
+// 设置 SDK 日志输出级别，详细分级请参见 setLogLevel 接口的说明
+tim.setLogLevel(0); // 普通级别，日志量较多，接入时建议使用
+// tim.setLogLevel(1); // release 级别，SDK 输出关键信息，生产环境时建议使用
+
+// 注册 COS SDK 插件
+tim.registerPlugin({ 'cos-wx-sdk': COS });
+
+App({
+
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function() {
+
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function(options) {
+
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function(msg) {
+
+  },
+  globalData: {
+
+    tim:tim
+
+  }
+
+
+})
---
+++ pages/index/index.js
@@ -0,0 +1,81 @@
+var app = getApp(); //启用全局变量，以通过实例
+
+
+
+Page({
+
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    genTestUserSig
+    console.log(app.globalData.tim)
+    let promise = app.globalData.tim.login({ userID: "sweetcoral", userSig: "123456" });
+    promise.then(function (imResponse) {
+      console.log(imResponse.data); // 登录成功
+      if (imResponse.data.repeatLogin === true) {
+        // 标识账号已登录，本次登录操作为重复登录。v2.5.1 起支持
+        console.log(imResponse.data.errorInfo);
+      }
+    }).catch(function (imError) {
+      console.warn('login error:', imError); // 登录失败的相关信息
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
